4fa3b4acec325383b40300ba446f24dd1082d9df
0053ishav <2002ishavs@gmail.com>
2026-08-10 23:25:50 +0530

    feat: add Telugu language

--- lib/languages/registry.ts
+++ lib/languages/registry.ts
@@ -2,10 +2,12 @@ import { gujarati } from "@/data/language/gujarati/language";
 import { hindi } from "@/data/language/hindi/language";
 import { panjabi } from "@/data/language/panjabi/language";
 import { tamil } from "@/data/language/tamil/language";
+import { telugu } from "@/data/language/telugu/language";
 
 export const languageRegistry = {
   hindi,
   panjabi,
   gujarati,
   tamil,
+  telugu,
 } as const;
