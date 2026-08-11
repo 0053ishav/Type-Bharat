df5d6fcbff588fc70e70b9240ec9f59d311795b0
Ishav <106452155+0053ishav@users.noreply.github.com>
2026-08-11 15:59:44 +0530

    feat: add Kannada language (#19)

---
+++ data/language/kannada/alphabet.ts
@@ -0,0 +1,84 @@
+import { Alphabet } from "@/types/alphabet";
+
+export const kannadaAlphabet: Alphabet = {
+  vowels: [
+    { letter: "ಅ", sound: "a" },
+    { letter: "ಆ", sound: "aa" },
+    { letter: "ಇ", sound: "i" },
+    { letter: "ಈ", sound: "ee" },
+    { letter: "ಉ", sound: "u" },
+    { letter: "ಊ", sound: "oo" },
+    { letter: "ಋ", sound: "ru" },
+    { letter: "ೠ", sound: "roo" },
+    { letter: "ಌ", sound: "lu" },
+    { letter: "ೡ", sound: "loo" },
+    { letter: "ಎ", sound: "e" },
+    { letter: "ಏ", sound: "ee" },
+    { letter: "ಐ", sound: "ai" },
+    { letter: "ಒ", sound: "o" },
+    { letter: "ಓ", sound: "oo" },
+    { letter: "ಔ", sound: "au" },
+  ],
+
+  consonants: [
+    { letter: "ಕ", sound: "ka" },
+    { letter: "ಖ", sound: "kha" },
+    { letter: "ಗ", sound: "ga" },
+    { letter: "ಘ", sound: "gha" },
+    { letter: "ಙ", sound: "nga" },
+
+    { letter: "ಚ", sound: "cha" },
+    { letter: "ಛ", sound: "chha" },
+    { letter: "ಜ", sound: "ja" },
+    { letter: "ಝ", sound: "jha" },
+    { letter: "ಞ", sound: "nya" },
+
+    { letter: "ಟ", sound: "ṭa" },
+    { letter: "ಠ", sound: "ṭha" },
+    { letter: "ಡ", sound: "ḍa" },
+    { letter: "ಢ", sound: "ḍha" },
+    { letter: "ಣ", sound: "ṇa" },
+
+    { letter: "ತ", sound: "ta" },
+    { letter: "ಥ", sound: "tha" },
+    { letter: "ದ", sound: "da" },
+    { letter: "ಧ", sound: "dha" },
+    { letter: "ನ", sound: "na" },
+
+    { letter: "ಪ", sound: "pa" },
+    { letter: "ಫ", sound: "pha" },
+    { letter: "ಬ", sound: "ba" },
+    { letter: "ಭ", sound: "bha" },
+    { letter: "ಮ", sound: "ma" },
+
+    { letter: "ಯ", sound: "ya" },
+    { letter: "ರ", sound: "ra" },
+    { letter: "ಲ", sound: "la" },
+    { letter: "ವ", sound: "va" },
+
+    { letter: "ಶ", sound: "sha" },
+    { letter: "ಷ", sound: "ṣha" },
+    { letter: "ಸ", sound: "sa" },
+    { letter: "ಹ", sound: "ha" },
+
+    { letter: "ಳ", sound: "ḷa" },
+  ],
+
+  extensions: [
+    { letter: "ಕ್ಷ", sound: "ksha" },
+    { letter: "ಜ್ಞ", sound: "gya" },
+  ],
+
+  numerals: [
+    { latin: "0", native: "೦" },
+    { latin: "1", native: "೧" },
+    { latin: "2", native: "೨" },
+    { latin: "3", native: "೩" },
+    { latin: "4", native: "೪" },
+    { latin: "5", native: "೫" },
+    { latin: "6", native: "೬" },
+    { latin: "7", native: "೭" },
+    { latin: "8", native: "೮" },
+    { latin: "9", native: "೯" },
+  ],
+};
